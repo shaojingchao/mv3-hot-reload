13032bcc75d18dcc3e69c796e7558949e1ba1276
shao <1247537710@qq.com>
2023-09-07 10:11:07 +0800

    feat: 改为 glob-watcher

--- README.md
+++ README.md
@@ -64,12 +64,9 @@ Example:
 
 ```js
 module.exports = {
-  // Specify the port of hot reload server, defaults to 9012
+  // defaults to 9012
   port: 9012,
-  // Specify the directory you want to watch, defaults to 'dist'
-  directory: 'dist',
-  // Specifies an array of filenames that should be excluded in watched directory
-  exclude: [],
+  globs: 'dist/*', // string || string[]
 }
 ```
 
--- bin/mv3-hot-reload.ts
+++ bin/mv3-hot-reload.ts
@@ -1,64 +1,66 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 
-import chokidar from 'chokidar'
-import WebSocket = require('ws')
-import debounce from 'lodash/debounce'
+import fs from 'fs'
 import path from 'path'
+import watch from 'glob-watcher'
+import WebSocket = require('ws')
 import { Message } from '../src/utils'
+
+function log(...arg: any[]) {
+  console.log('[MV3-HOT-RELOAD]:', ...arg)
+}
 
 interface Config {
   port?: number
-  directory?: string
-  exclude?: string[]
+  globs?: string | string[]
 }
 
 let port = 9012
-let directory = 'dist'
-let exclude: string[] = []
+let globs: string | string[] = ['./dist/*']
 const quiet = !!process.env.QUIET
-
+const configs = ['mv3-hot-reload.config.js', 'mv3-hot-reload.config.cjs', 'mv3-hot-reload.config.mjs']
 try {
-  const CONFIG_PATH = path.resolve('mv3-hot-reload.config.js')
+  let CONFIG_PATH = ''
+  configs.forEach((filename) => {
+    if (fs.existsSync(path.resolve(filename))) {
+      CONFIG_PATH = path.resolve(filename)
+    }
+  })
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const config: Config = require(CONFIG_PATH)
-
   port = config.port || port
-  directory = config.directory || directory
-  exclude = config.exclude || exclude
+  globs = config.globs || globs
 } catch (err) {
   // ignore
 }
 
-const directoryPath = path.resolve(directory)
-const excludePaths = exclude.map((file) => path.join(directoryPath, file))
-
 const wss = new WebSocket.Server({ port })
 
 wss.on('listening', () => {
-  console.log('hot reload server is listening...')
+  log('server is listening...')
 })
 
 wss.on('close', () => {
-  console.log('hot reload server closed.')
+  log('server closed.')
+})
+
+wss.on('error', () => {
+  log('server error.')
 })
 
 wss.on('connection', (ws) => {
-  const watcher = chokidar.watch(directoryPath, {
-    ignoreInitial: true,
+  log('server connected.')
+  const watcher = watch(globs, { ignoreInitial: true }, function (done) {
+    done()
   })
 
-  watcher.on(
-    'all',
-    debounce((_, path) => {
-      if (!excludePaths.includes(path)) {
-        if (!quiet) {
-          console.log(`File change detected. Path: ${path}`)
-        }
-        ws.send(Message.FileChange)
-      }
-    }, 500),
-  )
+  watcher.on('all', function (name, file) {
+    if (!quiet) {
+      log(`${name}`, file)
+    }
+    ws.send(Message.FileChange)
+  })
 
   ws.on('close', () => {
     watcher.close()
